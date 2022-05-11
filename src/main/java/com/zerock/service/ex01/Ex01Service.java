c81d86b8570a5ac724debbaafd112e50567af734
Eddy Kim <lasir3@gmail.com>
2022-05-11 09:49:03 +0900

    Service

---
+++ src/main/java/com/zerock/service/ex01/Ex01Service.java
@@ -0,0 +1,14 @@
+package com.zerock.service.ex01;
+
+import org.springframework.stereotype.Service;
+
+import com.zerock.controller.ex03.Customer;
+
+@Service
+public class Ex01Service {
+
+	public Customer getCustomer(int i) {
+		return null;
+	}
+
+}
---
+++ src/main/java/com/zerock/service/ex01/Ex02Service.java
@@ -0,0 +1,12 @@
+package com.zerock.service.ex01;
+
+import org.springframework.stereotype.Service;
+
+import com.zerock.controller.ex03.Employee;
+
+@Service
+public class Ex02Service {
+	public void insertEmployee(Employee e) {
+		
+	}
+}
